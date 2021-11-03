6029346609535640edb670d853fd35a495af9925
shentuzhigang <shentuzhigang@moresec.cn>
2021-11-03 20:31:09 +0800

    fix: update

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 import type { ViteBundlerOptions } from '@vuepress/bundler-vite'
 import type { StarThemeOptions } from '@starzkg/vuepress-theme-star'
-
+const isProd = process.env.NODE_ENV === 'production'
 export default defineUserConfig<StarThemeOptions, ViteBundlerOptions>({
   lang: 'zh-CN',
   title: 'Starzkg',
@@ -10,4 +10,9 @@ export default defineUserConfig<StarThemeOptions, ViteBundlerOptions>({
   themeConfig: {
     logo: '/logo.png',
   },
+  bundler:
+  // specify bundler via environment variable
+      process.env.DOCS_BUNDLER ??
+      // use vite in dev, use webpack in prod
+      (isProd ? '@vuepress/webpack' : '@vuepress/vite'),
 })
